4f7df6e9e5db69179c32004c809372a7eb3606a6
Yash <yash999901@gmail.com>
2024-09-25 13:46:23 +0530

    Data Structure and Algorithm

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+	int a= 2, b=4; 
+    int g =0;
+	cout<<"Input:";
+    cin>>g;
+	cout<<a+g<<endl; 
+	cout<<"lauda lelo mera"<<endl;
+	return 0; 
+}
