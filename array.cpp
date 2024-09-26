2324c962dc88323183be7affb8a364bcebe1b71c
Yash <yash999901@gmail.com>
2024-09-27 02:46:22 +0530

    Data Structure and Algorithm

---
+++ array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ; 
+int main(){
+    
+    int a[5] = {1, 2, 3, 4, 5};
+
+    cout << a[0] << endl; // 1
+    cout << a[1] << endl; // 2
+    cout << a[2] << endl; // 3
+    cout << a[3] << endl; // 4
+    cout << a[4] << endl; // 5
+
+    
+    return 0;
+}
